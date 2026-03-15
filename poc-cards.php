30caf67e2524940dd8769c921175a11e77e24a79
Esitsolf <esitsolf@hotmail.fr>
2026-03-15 10:55:16 +0100

    include changes on main

--- poc-cards.php
+++ poc-cards.php
@@ -3,7 +3,7 @@
  * Plugin Name: Action Cards
  * Plugin URI:  https://pioneersofchange.org
  * Description: Action cards for Pioneers of Change — Custom Post Type, ACF fields, and shortcode renderer.
- * Version:     beta 1.0.8
+ * Version:      1.1.0
  * Author:      Pioneers of Change
  * Text Domain: actionskarten
  */
